b30905173d2131c9fdb2244699f1caf08de0114e
omy-q <olya_aparina@mail.ru>
2021-06-07 15:56:09 +0300

    add init to numbers buttons

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,18 +3,43 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView text;
 
+    private final View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            text.append(((Button) v).getText());
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initTextView();
+        initNumbersButton();
         
+    }
+
+    private  void initNumbersButton(){
+
+        findViewById(R.id.button_0).setOnClickListener(listener);
+        findViewById(R.id.button_1).setOnClickListener(listener);
+        findViewById(R.id.button_2).setOnClickListener(listener);
+        findViewById(R.id.button_3).setOnClickListener(listener);
+        findViewById(R.id.button_4).setOnClickListener(listener);
+        findViewById(R.id.button_5).setOnClickListener(listener);
+        findViewById(R.id.button_6).setOnClickListener(listener);
+        findViewById(R.id.button_7).setOnClickListener(listener);
+        findViewById(R.id.button_8).setOnClickListener(listener);
+        findViewById(R.id.button_9).setOnClickListener(listener);
+
     }
 
     private void initTextView() {
