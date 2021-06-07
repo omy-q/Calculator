c006fe4a2774ce3d3c469cd9c88285c9d2863644
omy-q <olya_aparina@mail.ru>
2021-06-07 15:50:55 +0300

    add init to TextView

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initTextView();
+        
+    }
+
+    private void initTextView() {
+        text = findViewById(R.id.text_view);
     }
 }
