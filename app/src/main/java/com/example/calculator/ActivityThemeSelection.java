bbf6d8b5be5c2dd954ac378f429929f1d49b7b66
omy-q <olya_aparina@mail.ru>
2021-06-16 19:28:44 +0300

    add init all elements and preparing to send data to MainActivity

--- app/src/main/java/com/example/calculator/ActivityThemeSelection.java
+++ app/src/main/java/com/example/calculator/ActivityThemeSelection.java
@@ -2,13 +2,83 @@ package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class ActivityThemeSelection extends AppCompatActivity {
+
+    private final String themeName = "THEME_NAME";
+    private String themeValue;
+    private final View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {}
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_theme_selection);
+
+        initImagePurpleButton();
+        initImageBlueButton();
+        initImageGreenButton();
+        initImageDarkButton();
+        initBackButton();
+    }
+
+    private void initImagePurpleButton() {
+
+        findViewById(R.id.image_purple).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("MainActivity", "PurpleTheme");
+                themeValue = "PurpleTheme";
+            }
+        });
+    }
+
+    private void initImageDarkButton() {
+        findViewById(R.id.image_dark).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("MainActivity", "DarkTheme");
+                themeValue = "DarkTheme";
+            }
+        });
+    }
+
+    private void initImageGreenButton() {
+        findViewById(R.id.image_green).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("MainActivity", "GreenTheme");
+                themeValue = "GreenTheme";
+            }
+        });
+    }
+
+    private void initImageBlueButton() {
+        findViewById(R.id.image_green).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("MainActivity", "BlueTheme");
+                themeValue = "BlueTheme";
+            }
+        });
+    }
+
+    private void initBackButton() {
+
+        findViewById(R.id.back_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent infoThemeIntent = new Intent(ActivityThemeSelection.this, MainActivity.class);
+                infoThemeIntent.putExtra(themeName, themeValue);
+                setResult(RESULT_OK, infoThemeIntent);
+                finish();
+            }
+        });
     }
 }
